5c1df73e0e12e8f2c8e461afab551945f2e0f7d5
Olga Skurativska <olga.skurativska@homegate.ch>
2020-07-02 09:29:41 +0200

    demos: aria in modal

---
+++ 04-aria/05-modal/solution/src/components/OrderForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside aria-labelledby="orderForm">
+    <h2 id="orderForm"><a name="order-form"></a>Order</h2>
+    <form action="">
+      <div>
+        <label for="size">Size</label>
+        <select name="size" id="size">
+          <option value="xs">S</option>
+          <option value="s">S</option>
+          <option value="m">M</option>
+          <option value="l">L</option>
+          <option value="xl">L</option>
+        </select>
+      </div>
+      <div>
+        <label for="color">Color</label>
+        <select name="color" id="color">
+          <option value="black">Black</option>
+          <option value="white">White</option>
+          <option value="pink">Pink</option>
+        </select>
+      </div>
+      <div>
+        <label for="quantity">Quantity</label>
+        <input type="number" id="quantity" name="quantity" value="1" />
+      </div>
+      <button type="button" class="cta" @click="$emit('submitForm')">Place an order</button>
+    </form>
+  </aside>
+</template>
---
+++ 04-aria/05-modal/solution/src/components/Product.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <img src="/product.jpg" />
+    <div class="description">
+      <section>
+        <h1><a name="product-description"></a>Cute cat sleep mask</h1>
+        <p>CHF 99.95</p>
+        <p>
+          Debug your websites for accessibility issues like never before with this fluffy sleep
+          mask. Experience the web the way screen reader users do!
+        </p>
+        <p>
+          A <a href="https://en.wikipedia.org/wiki/Screen_reader">screen reader</a> is a form of
+          assistive technology (AT) that renders text and image content as speech or braille output.
+          Screen readers are essential to people who are blind, and are useful to people who are
+          visually impaired, illiterate, or have a learning disability.
+        </p>
+        <p>
+          Screen readers are software applications that attempt to convey what people with normal
+          eyesight see on a display to their users via non-visual means, like text-to-speech, sound
+          icons, or a Braille device. They do this by applying a wide variety of techniques that
+          include, for example, interacting with dedicated accessibility APIs, using various
+          operating system features (like inter-process communication and querying user interface
+          properties), and employing hooking techniques.
+        </p>
+      </section>
+      <OrderForm @submitForm="$emit('order')"></OrderForm>
+    </div>
+  </main>
+</template>
+
+<script>
+import OrderForm from './OrderForm.vue';
+
+export default {
+  name: 'Product',
+  components: {
+    OrderForm
+  },
+  props: {
+    isShoppingCartModalOpen: Boolean
+  }
+};
+</script>
